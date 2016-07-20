f5fff3bada721af18e92353a95af832a83ea6c55
James H. Hill <hilljh82@gmail.com>
2016-07-19 21:56:00 -0400

    Collection of HostnameVerifier objects

---
+++ lib/src/main/java/com/onehilltech/selftrust/HostnameVerifiers.java
@@ -0,0 +1,36 @@
+package com.onehilltech.selftrust;
+
+import java.util.HashSet;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+public class HostnameVerifiers implements HostnameVerifier
+{
+  private HashSet <HostnameVerifier> verifiers_ = new HashSet<> ();
+
+  public HostnameVerifiers ()
+  {
+
+  }
+
+  public boolean add (HostnameVerifier verifier)
+  {
+    return this.verifiers_.add (verifier);
+  }
+
+  public boolean remove (HostnameVerifier verifier)
+  {
+    return this.verifiers_.remove (verifier);
+  }
+
+  @Override
+  public boolean verify (String hostname, SSLSession session)
+  {
+    for (HostnameVerifier verifier : this.verifiers_)
+      if (verifier.verify (hostname, session))
+        return true;
+
+    return false;
+  }
+}
